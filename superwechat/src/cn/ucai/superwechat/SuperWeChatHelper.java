ca13d180d926316df1ab006c0fbe36623d9d2aa9
gehaiping <804563299@qq.com>
2017-02-09 20:54:26 +0800

    退出登录，清空内存中的数据

--- superwechat/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ superwechat/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -1281,6 +1281,7 @@ public class SuperWeChatHelper {
         isGroupAndContactListenerRegisted = false;
 
         setContactList(null);
+        setAppContactList(null);
         setRobotList(null);
         getUserProfileManager().reset();
         SuperWeChatDBManager.getInstance().closeDB();
@@ -1334,5 +1335,19 @@ public class SuperWeChatHelper {
         }
 
         return appContactList;
+    }
+
+    /**
+     * update user list to cache and database
+     *
+     * @param contactInfoList
+     */
+    public void updateAppContactList(List<User> contactInfoList) {
+        for (User u : contactInfoList) {
+            appContactList.put(u.getMUserName(), u);
+        }
+        ArrayList<User> mList = new ArrayList<User>();
+        mList.addAll(appContactList.values());
+        demoModel.saveAppContactList(mList);
     }
 }
