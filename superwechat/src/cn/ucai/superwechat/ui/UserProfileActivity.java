22721cccdeda31e3a5fd42641df4cbdfa52b3705
gehaiping <804563299@qq.com>
2017-02-13 15:46:29 +0800

    更新用户头像并同步数据库

--- superwechat/src/cn/ucai/superwechat/ui/UserProfileActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/UserProfileActivity.java
@@ -25,9 +25,15 @@ import com.hyphenate.EMValueCallBack;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.domain.EaseUser;
 import com.hyphenate.easeui.domain.User;
+import com.hyphenate.easeui.utils.EaseImageUtils;
 import com.hyphenate.easeui.utils.EaseUserUtils;
 
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -225,7 +231,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
                 break;
             case REQUESTCODE_CUTTING:
                 if (data != null) {
-                    setPicToView(data);
+                    uploadAppUserAvatar(data);
                 }
                 break;
             default:
@@ -261,6 +267,66 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
             uploadUserAvatar(Bitmap2Bytes(photo));
         }
 
+    }
+
+    private void uploadAppUserAvatar(Intent pidata) {
+        File file = saveBitmapFile(pidata);
+        L.e(TAG, "file=" + file);
+        if (file == null) {
+            return;
+        }
+        L.e(TAG, "file =" + file.getAbsolutePath());
+        dialog = ProgressDialog.show(this, getString(R.string.dl_update_photo), getString(R.string.dl_waiting));
+        NetDao.uploadUserAvatar(this, EMClient.getInstance().getCurrentUser(), file,
+                new OnCompletListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        L.e(TAG, "s=" + s);
+                        if (s != null) {
+                            Result result = ResultUtils.getResultFromJson(s, User.class);
+                            if (result != null) {
+                                if (result.isRetMsg()) {
+                                    User user = (User) result.getRetData();
+                                    if (user != null) {
+                                        PreferenceManager.getInstance().setCurrentUserAvatar(user.getAvatar());
+                                        SuperWeChatHelper.getInstance().saveAppContact(user);
+                                        EaseUserUtils.setUserAvatar(UserProfileActivity.this,
+                                                user.getMUserName(), mIvUserinfoAvatar);
+                                        CommonUtils.showShortToast(R.string.toast_updatephoto_success);
+                                    }
+                                }
+                            }
+                        }
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        L.e(TAG, "error=" + error);
+                        dialog.dismiss();
+                        CommonUtils.showShortToast(R.string.toast_updatephoto_fail);
+                    }
+                });
+    }
+
+    private File saveBitmapFile(Intent pidata) {
+        Bundle extras = pidata.getExtras();
+        if (extras != null) {
+            Bitmap bitmap = extras.getParcelable("data");
+            String imagePath = EaseImageUtils.getImagePath(EMClient.getInstance().getCurrentUser() + I.AVATAR_SUFFIX_JPG);
+            File file = new File(imagePath);//将要保存图片的路径
+            L.e("file path=" + file.getAbsolutePath());
+            try {
+                BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                bos.flush();
+                bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return file;
+        }
+        return null;
     }
 
     private void uploadUserAvatar(final byte[] data) {
