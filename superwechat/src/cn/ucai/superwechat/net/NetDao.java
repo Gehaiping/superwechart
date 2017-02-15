0e79831c9a77e66e35ffa67ebfdca9df93ea2f32
gehaiping <804563299@qq.com>
2017-02-15 19:11:54 +0800

    初始化联系人数据

--- superwechat/src/cn/ucai/superwechat/net/NetDao.java
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -85,4 +85,11 @@ public class NetDao {
                 .execute(listener);
     }
 
+    public static void loadContact(Context context, String username, OnCompletListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_DOWNLOAD_CONTACT_ALL_LIST)
+                .addParam(I.Contact.USER_NAME, username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
 }
