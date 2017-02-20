3c95b3df6c5311bbf6fe98a0161be0041c99ae38
gehaiping <804563299@qq.com>
2017-02-20 17:16:22 +0800

    群组详情中点击自己跳转至个人详细资料

--- superwechat/src/cn/ucai/superwechat/utils/MFGT.java
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.domain.User;
 
 import cn.ucai.superwechat.I;
@@ -77,8 +78,12 @@ public class MFGT {
     }
 
     public static void gotoFriend(Activity activity, String username) {
-        startActivity(activity, new Intent(activity, FriendProfileActivity.class)
-                .putExtra(I.User.USER_NAME, username));
+        if (username.equals(EMClient.getInstance().getCurrentUser())) {
+            gotoUserProfile(activity);
+        } else {
+            startActivity(activity, new Intent(activity, FriendProfileActivity.class)
+                    .putExtra(I.User.USER_NAME, username));
+        }
     }
 
     public static void gotoAddFriend(Activity activity, String userName) {
