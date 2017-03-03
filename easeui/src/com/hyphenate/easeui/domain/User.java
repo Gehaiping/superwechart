e43e8e6187b48b11bee2e328833d6e1e8278a83c
gehaiping <804563299@qq.com>
2017-03-03 14:47:59 +0800

    更改服务器地址

--- easeui/src/com/hyphenate/easeui/domain/User.java
+++ easeui/src/com/hyphenate/easeui/domain/User.java
@@ -1,7 +1,5 @@
 package com.hyphenate.easeui.domain;
 
-import com.hyphenate.easeui.utils.EaseCommonUtils;
-
 import java.io.Serializable;
 
 public class User implements Serializable {
@@ -132,7 +130,7 @@ public class User implements Serializable {
 	}
 
 	public String getAvatar() {
-		String path = "http://101.251.196.90:8000/SuperWeChatServerV2.0/downloadAvatar?name_or_hxid="+getMUserName()+"&avatarType=user_avatar&m_avatar_suffix="+getMAvatarSuffix()+"&updatetime="+getMAvatarLastUpdateTime();
+		String path = "http://101.251.196.91:8888/SuperWeChatServerV2.0/downloadAvatar?name_or_hxid=" + getMUserName() + "&avatarType=user_avatar&m_avatar_suffix=" + getMAvatarSuffix() + "&updatetime=" + getMAvatarLastUpdateTime();
 		return path;
 	}
 }
